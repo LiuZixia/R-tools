a85fcc757096260e9dd352b6e435d92337a8e1d8
Zixia Liu <61141162+LiuZixia@users.noreply.github.com>
2020-05-29 18:23:35 +0200

    Update Install-CentOS7.sh

--- Rstudio-Server/Install-CentOS7.sh
+++ Rstudio-Server/Install-CentOS7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Install Rstudio-Server (v1.3.959) with custom version.\nBy LiuZixia@GitHub \nUrl: https://github.com/LiuZixia/R-tools"
+echo -e "Install Rstudio-Server (v1.3.959) with custom version.\nBy LiuZixia@GitHub \nUrl: https://github.com/LiuZixia/R-tools"
 read -p "Enter R version [3.6.3]: " R_VERSION
 R_VERSION=${R_VERSION:-3.6.3}
 read -p "Enter Rstudio-Server Web Interface port [80]: " RSTUDIO_PORT
