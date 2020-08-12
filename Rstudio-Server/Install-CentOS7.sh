48fc644095be7dbe248befd44edd13d594046198
Zixia Liu <61141162+LiuZixia@users.noreply.github.com>
2020-08-12 19:44:00 +0800

    Update Install-CentOS7.sh

--- Rstudio-Server/Install-CentOS7.sh
+++ Rstudio-Server/Install-CentOS7.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo -e "Install Rstudio-Server (v1.3.959) with custom version.\nBy LiuZixia@GitHub \nUrl: https://github.com/LiuZixia/R-tools"
-read -p "Enter R version [3.6.3]: " R_VERSION
-R_VERSION=${R_VERSION:-3.6.3}
-read -p "Enter Rstudio-Server Web Interface port [80]: " RSTUDIO_PORT
-RSTUDIO_PORT=${RSTUDIO_PORT:-80}
+R_VERSION=3.6.3
 yum install sudo epel-release -y
 sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 sudo subscription-manager repos --enable "rhel-*-optional-rpms" -y
@@ -16,7 +13,7 @@ sudo useradd -p "$pwhash" rstudio -d /data
 chown -R rstudio /data
 wget https://download2.rstudio.org/server/centos6/x86_64/rstudio-server-rhel-1.3.959-x86_64.rpm
 sudo yum install rstudio-server-rhel-1.3.959-x86_64.rpm -y
-echo "www-port=${RSTUDIO_PORT}" >> /etc/rstudio/rserver.conf
+echo "www-port=80" >> /etc/rstudio/rserver.conf
 echo "www-address=0.0.0.0" >> /etc/rstudio/rserver.conf
 sudo rstudio-server restart
 firewall-cmd --zone=public --add-port=${RSTUDIO_PORT}/tcp --permanent
