83e827d19cd6998634c7ae246f3d4154147f7510
Zixia Liu <61141162+LiuZixia@users.noreply.github.com>
2021-09-23 11:36:40 +0200

    Update install-r-ubuntu.sh

--- Rstudio-Server/install-r-ubuntu.sh
+++ Rstudio-Server/install-r-ubuntu.sh
@@ -5,4 +5,4 @@ add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_relea
 apt install r-base-dev -y
 apt install gdebi-core -y
 wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.4.1717-amd64.deb
-gdebi rstudio-server-1.4.1717-amd64.deb
+yes | gdebi rstudio-server-1.4.1717-amd64.deb
