a206557981a2b280ce7402ba9928dab58ad4380d
Zixia Liu <61141162+LiuZixia@users.noreply.github.com>
2021-09-23 11:32:26 +0200

    Update install-r-ubuntu.sh

--- Rstudio-Server/install-r-ubuntu.sh
+++ Rstudio-Server/install-r-ubuntu.sh
@@ -2,7 +2,7 @@
 apt install --no-install-recommends software-properties-common dirmngr -y
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-apt install r-base -y
+apt install r-base-dev -y
 apt install gdebi-core -y
 wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.4.1717-amd64.deb
 gdebi rstudio-server-1.4.1717-amd64.deb
